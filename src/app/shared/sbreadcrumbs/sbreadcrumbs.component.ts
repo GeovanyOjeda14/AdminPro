8fb7ab657ac435fbc7bb5433971e33600695a1a4
GeovanyOjeda14 <geovanyojeda14@gmail.com>
2020-01-17 08:42:31 -0500

    primer commit

---
+++ src/app/shared/sbreadcrumbs/sbreadcrumbs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sbreadcrumbs',
+  templateUrl: './sbreadcrumbs.component.html',
+  styles: []
+})
+export class SbreadcrumbsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
